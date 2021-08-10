850f5abd4dbba70a8e824958bc5d0aa23e6e6345
Azhe Kun <azhe403@gmail.com>
2021-08-10 20:47:47 +0700

    git init

---
+++ HelloTelegramBot/Handlers/PingHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using BotFramework;
+using BotFramework.Attributes;
+using BotFramework.Setup;
+using BotFramework.Utils;
+using HelloTelegramBot.Utils;
+
+namespace HelloTelegramBot.Handlers
+{
+    public class PingHandler: BotEventHandler
+    {
+        [Command("ping", CommandParseMode.Both)]
+        public async Task Ping()
+        {
+            var currProcess = Process.GetCurrentProcess();
+            var processStartTime = currProcess.StartTime;
+            var processUptime = (DateTime.Now - processStartTime);
+
+            await Bot.SendTextMessageAsync(Chat, $"Pong!\nUptime: {processUptime.ToHumanDuration()}");
+        }
+    }
+}

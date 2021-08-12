6f39bd62d82853228e29c74527105bd53c2e1cd6
Azhe Kun <azhe403@gmail.com>
2021-08-12 19:26:59 +0700

    add update handler

---
+++ HelloTelegramBot/Handlers/UpdateHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using BotFramework;
+using BotFramework.Attributes;
+using BotFramework.Enums;
+using BotFramework.Setup;
+using Serilog;
+
+namespace HelloTelegramBot.Handlers
+{
+    public class UpdateHandler : ZiziEventHandler
+    {
+        [Update(InChat.All, UpdateFlag.All)]
+        [Priority(10)]
+        public async Task<bool> OnUpdate()
+        {
+            await Bot.SendTextMessageAsync(Chat,"New Update!" +
+                                                $"\nTime: {TimeInit}");
+
+            return true;
+        }
+    }
+}

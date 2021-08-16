3826132e1d09ae5b310e08f1c6abcbc6c8b258eb
Azhe-kun <azhe403@gmail.com>
2021-08-16 20:41:59 +0700

    handle null property

--- HelloTelegramBot/Handlers/ZiziEventHandler.cs
+++ HelloTelegramBot/Handlers/ZiziEventHandler.cs
@@ -1,12 +1,21 @@
-﻿using BotFramework;
+using System;
+using System.Threading.Tasks;
+using BotFramework;
+using BotFramework.Utils;
 using HelloTelegramBot.Utils;
+using Serilog;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace HelloTelegramBot.Handlers
 {
     public abstract class ZiziEventHandler : BotEventHandler
     {
-        protected string TimeInit => RawUpdate.Message.Date.GetDelay();
-        protected string TimeProc => RawUpdate.Message.Date.GetDelay();
+        protected string TimeInit => RawUpdate.Message == null ? string.Empty : RawUpdate.Message.Date.GetDelay();
+        protected string TimeProc => RawUpdate.Message == null ? string.Empty : RawUpdate.Message.Date.GetDelay();
+
         protected bool HasUsername => From.Username != null;
+
     }
 }

0e59da9cdcdac6423e66f04b79e04187b6dd4a19
Azhe-kun <azhe403@gmail.com>
2021-08-16 20:44:21 +0700

    add jsonUtil

---
+++ HelloTelegramBot/Utils/JsonUtil.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace HelloTelegramBot.Utils
+{
+    public static class JsonUtil
+    {
+        public static string ToJson<T>(this T any, bool indented = false, bool includeNull = false)
+        {
+            return JsonConvert.SerializeObject(any, new JsonSerializerSettings()
+            {
+                Formatting = indented ? Formatting.Indented : Formatting.None,
+                NullValueHandling = includeNull ? NullValueHandling.Include : NullValueHandling.Ignore
+            });
+        }
+    }
+}

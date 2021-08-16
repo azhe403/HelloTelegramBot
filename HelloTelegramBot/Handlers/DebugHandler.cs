37e3de76a4f334bbd3b18130d2614b55a3d6beb8
Azhe-kun <azhe403@gmail.com>
2021-08-16 20:47:37 +0700

    add /dbg for debug json

---
+++ HelloTelegramBot/Handlers/DebugHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using BotFramework.Attributes;
+using BotFramework.Setup;
+using BotFramework.Utils;
+using HelloTelegramBot.Utils;
+
+namespace HelloTelegramBot.Handlers
+{
+    public class DebugHandler : ZiziEventHandler
+    {
+        [Command("dbg", CommandParseMode.Both)]
+        public async Task Debug()
+        {
+            var update = RawUpdate.ToJson(indented: true);
+
+            var htmlStr = new HtmlString()
+                .Bold("Debug Message").Br()
+                .Code(update).Br();
+
+            await SendMessageTextAsync(htmlStr);
+        }
+    }
+}
